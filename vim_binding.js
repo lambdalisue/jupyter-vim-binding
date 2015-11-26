d60664be7425853541517c565e5017649e669a01
lambdalisue <lambdalisue@hashnote.net>
2015-11-26 21:41:21 +0900

    Fix key binding issue on Vim's command mode
    It fix #15

--- vim_binding.js
+++ vim_binding.js
@@ -17,7 +17,13 @@
  */
 "use strict";
 
-define(['base/js/namespace', 'notebook/js/cell', 'codemirror/keymap/vim'], function(namespace, cell) {
+define([
+  'base/js/namespace',
+  'notebook/js/notebook',
+  'notebook/js/cell',
+  'codemirror/keymap/vim'
+], function(namespace, notebook, cell) {
+  var undefined;
   var extend = function(destination, source) {
     for (var property in source) {
       if (source.hasOwnProperty(property)) {
@@ -26,42 +32,62 @@ define(['base/js/namespace', 'notebook/js/cell', 'codemirror/keymap/vim'], funct
     }
     return destination;
   };
-  var leaveInsert = function(cm) {
-    CodeMirror.Vim.handleKey(cm, '<Esc>');
-  };
-  var leaveInsertOrNormal = function(cm) {
-    if (cm.state.vim.insertMode || cm.state.vim.visualMode) {
-      CodeMirror.Vim.handleKey(cm, '<Esc>');
-    } else {
-      namespace.notebook.command_mode();
-      namespace.notebook.focus_cell();
-    }
-  };
-  var insertSoftTab = function(cm) {
-    cm.execCommand('insertSoftTab');
-  }
 
-  // Override save method of CodeMirror to save a checkpoint
+  //
+  // Extend CodeMirror
+  //
   CodeMirror.prototype.save = function() {
     namespace.notebook.save_checkpoint();
   };
-
-  // Override default config of CodeMirror to enable Vim binding
-  var options = cell.Cell.options_default;
-  options.cm_config.keyMap = 'vim';
-  options.cm_config.extraKeys = extend(options.cm_config.extraKeys || {}, {
-    'Esc': leaveInsertOrNormal,
-    'Tab': insertSoftTab,
-    'Ctrl-C': false,
-  });
-
-  // Override 'bind_events' to ensure 'Normal' mode on blur
-  var original_bind_events = cell.Cell.prototype.bind_events;
-  cell.Cell.prototype.bind_events = function() {
-    original_bind_events.apply(this);
-    if (this.code_mirror) {
-      this.code_mirror.on('blur', leaveInsert);
+  CodeMirror.Vim.Jupyter = {};
+  CodeMirror.Vim.Jupyter.leaveInsertMode = function(cm) {
+    CodeMirror.Vim.handleKey(cm, '<Esc>');
+  };
+  CodeMirror.Vim.Jupyter.leaveNormalMode = function(cm) {
+    namespace.notebook.command_mode();
+    namespace.notebook.focus_cell();
+  };
+  CodeMirror.Vim.Jupyter.leaveInsertOrNormalMode = function(cm) {
+    if (cm.state.vim.insertMode || cm.state.vim.visualMode) {
+      CodeMirror.Vim.Jupyter.leaveInsertMode(cm);
+    } else {
+      CodeMirror.Vim.Jupyter.leaveNormalMode(cm);
     }
+  };
+  CodeMirror.Vim.Jupyter.insertSoftTab = function(cm) {
+    cm.execCommand('insertSoftTab');
+  };
+  CodeMirror.Vim.Jupyter.init = function() {
+    var config = cell.Cell.options_default.cm_config;
+    config.keyMap = 'vim';
+    config.extraKeys = extend(config.extraKeys || {}, {
+      'Esc': CodeMirror.Vim.Jupyter.leaveInsertOrNormalMode,
+      'Tab': CodeMirror.Vim.Jupyter.insertSoftTab,
+      'Ctrl-C': false,  // To enable clipboard copy
+    });
+  };
+  // Initialize
+  CodeMirror.Vim.Jupyter.init();
+
+  // Extend Jupyter
+  var original = {};
+  original.handle_command_mode = notebook.Notebook.prototype.handle_command_mode;
+  original.handle_edit_mode = notebook.Notebook.prototype.handle_edit_mode;
+
+  notebook.Notebook.prototype.handle_command_mode = function(cell) {
+    if (document.querySelector('.CodeMirror-dialog')) {
+      // .CodeMirror-dialog exists, mean that user hit ':' to enter Vim's
+      // command mode so do not leave Jupyter's edit mode in this case
+      return;
+    }
+    original.handle_command_mode.call(this, cell);
+  };
+  notebook.Notebook.prototype.handle_edit_mode = function(cell) {
+    // Make sure that the CodeMirror is in Vim's Normal mode
+    if (cell.code_mirror) {
+      CodeMirror.Vim.Jupyter.leaveInsertMode(cell.code_mirror);
+    }
+    original.handle_edit_mode.call(this, cell);
   };
 
   // Register custom actions
@@ -139,35 +165,26 @@ define(['base/js/namespace', 'notebook/js/cell', 'codemirror/keymap/vim'], funct
   km.command_shortcuts.add_shortcut('alt-enter', 'ipython.execute-and-insert-after');
   km.command_shortcuts.add_shortcut('ctrl-enter', 'ipython.execute-in-place');
   km.command_shortcuts.add_shortcut('shift-enter', 'ipython.run-select-next');
-
-  // Keys defined to 'command_shortcuts' would steal pressed keys in CodeMirror's Vim command mode
-  // Thus the following shortcuts, which is not often used, are not defined even it is defined in
-  // normal IPython/Jupyter.
-  // See https://github.com/lambdalisue/jupyter-vim-binding/issues/2
-  //km.command_shortcuts.add_shortcut('0,0', 'ipython.restart-kernel');
-  //km.command_shortcuts.add_shortcut('1', 'ipython.change-selected-cell-to-heading-1');
-  //km.command_shortcuts.add_shortcut('2', 'ipython.change-selected-cell-to-heading-2');
-  //km.command_shortcuts.add_shortcut('3', 'ipython.change-selected-cell-to-heading-3');
-  //km.command_shortcuts.add_shortcut('4', 'ipython.change-selected-cell-to-heading-4');
-  //km.command_shortcuts.add_shortcut('5', 'ipython.change-selected-cell-to-heading-5');
-  //km.command_shortcuts.add_shortcut('6', 'ipython.change-selected-cell-to-heading-6');
+  km.command_shortcuts.add_shortcut('0,0', 'ipython.restart-kernel');
+  km.command_shortcuts.add_shortcut('1', 'ipython.change-selected-cell-to-heading-1');
+  km.command_shortcuts.add_shortcut('2', 'ipython.change-selected-cell-to-heading-2');
+  km.command_shortcuts.add_shortcut('3', 'ipython.change-selected-cell-to-heading-3');
+  km.command_shortcuts.add_shortcut('4', 'ipython.change-selected-cell-to-heading-4');
+  km.command_shortcuts.add_shortcut('5', 'ipython.change-selected-cell-to-heading-5');
+  km.command_shortcuts.add_shortcut('6', 'ipython.change-selected-cell-to-heading-6');
 
   var exports = {
     'load_ipython_extension': function() {
-      var options = cell.Cell.options_default;
-      // apply options and events on existing CodeMirror instances
+      // Apply default CodeMirror config to existing CodeMirror instances
+      var config = cell.Cell.options_default.cm_config;
       namespace.notebook.get_cells().map(function(cell) {
         var cm = cell.code_mirror;
         if (cm) {
-          cm.setOption('keyMap', options.cm_config.keyMap);
-          cm.setOption('extraKeys', options.cm_config.extraKeys);
-          cm.on('blur', leaveInsert);
+          cm.setOption('keyMap', config.keyMap);
+          cm.setOption('extraKeys', config.extraKeys);
         }
       });
     },
-    'leaveInsert': leaveInsert,
-    'leaveInsertOrNormal': leaveInsertOrNormal,
-    'insertSoftTab': insertSoftTab,
   };
   return exports;
 });
